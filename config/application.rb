04696a44508a501bad6cfc0753e65bba1446bf6f
George Zhukov <scofield.cross@gmail.com>
2015-08-30 20:53:52 +0300

    removed unused helpers

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,5 @@ module AwesomeTodo
     config.assets.paths << Rails.root.join("vendor","assets","bower_components")
     config.assets.paths << Rails.root.join("vendor","assets","bower_components","bootstrap-sass-official","assets","fonts")
     config.assets.precompile << %r(.*.(?:eot|svg|ttf|woff)$)
-    
   end
 end
