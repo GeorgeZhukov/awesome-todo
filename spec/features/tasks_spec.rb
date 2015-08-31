7a1f9b6877bcfe8b1e936712e74f7b58fe754328
George Zhukov <scofield.cross@gmail.com>
2015-08-31 16:18:03 +0300

    added few feature tests

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -15,4 +15,30 @@ RSpec.feature "Tasks", type: :feature, js: true do
 
     expect(page).to have_content task.title
   end
+
+  scenario "A user can add a new task" do
+    visit '/#/sign-in'
+    within "#sign-in-form" do
+      fill_in "E-mail", with: user.email
+      fill_in "Password", with: "password"
+      click_button "Sign In"
+    end
+
+    task_attrs = attributes_for :task
+    fill_in "New task", with: task_attrs[:title]
+    click_button "Add"
+    expect(page).to have_content task_attrs[:title]
+  end
+
+  scenario "A user can remove the task" do
+    visit '/#/sign-in'
+    within "#sign-in-form" do
+      fill_in "E-mail", with: user.email
+      fill_in "Password", with: "password"
+      click_button "Sign In"
+    end
+
+    click_button "Remove"
+    expect(page).not_to have_content task.title
+  end
 end
