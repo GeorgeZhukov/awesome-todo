c7f264ee9e6463719da98d2fde98ed6071869a84
George Zhukov <scofield.cross@gmail.com>
2015-09-02 14:19:46 +0300

    added position to tasks

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150901143810) do
+ActiveRecord::Schema.define(version: 20150902092707) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20150901143810) do
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "position"
   end
 
   add_index "tasks", ["project_id"], name: "index_tasks_on_project_id", using: :btree
